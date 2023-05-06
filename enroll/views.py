0381e283b34e0617ee358dae8469cd40c4f57ce1
sukhpal <sukhpal948@gmail.com>
2023-05-06 14:15:54 -0400

    first

---
+++ enroll/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# from enroll1.models import * 
+# from rest_framework.views import APIView
+# from django.http import JsonResponse
+
+# Create your views here.
+ 
+def home1(request):
+    return render(request,'enroll/main.html')
+
+
+# Create your views here.
